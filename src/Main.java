9908b54fbf1b0a6d3f09439f1338d52321ae954d
JCzay <Julian.Czaja99@icloud.com>
2021-12-15 19:15:15 +0100

    ćw. 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
+
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+
+        System.out.println("Witam Świecie!");
+        System.out.println("Od dzisiaj zaczynam naukę języka JAVA!");
+        System.out.println("Mam nadzieję, że w końcu nauczę się programowania!");
+        System.out.println("A w nadchodzącym roku" + " będę mógł " + "zmienić pracę!");
+
+        System.out.print("Liczę na przypływ motywacji");
+        System.out.print(" ");
+        System.out.print("oraz wytrwałość w dążeniu do celu!");
+
+        System.out.printf(" ");
+        System.out.printf("LETS GO!!!");
 
     }
 }
