b1b3d560dcd90d062be57b188dcf2ab1015eb973
Julian <Julian.Czaja99@icloud.com>
2021-12-17 21:23:25 +0100

    ćw. 4

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,28 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+        boolean a = true;
+        boolean b = false;
+
+        int x = 5;
+        int y = 55;
+        int z = 555;
+
+        System.out.println(x > y);
+        System.out.println(x < y);
+        System.out.println(x > z);
+        System.out.println(x < z);
+        System.out.println(true && false);
+        System.out.println(true && true);
+        System.out.println(false && false);
+        System.out.println(true || false);
+        System.out.println(true || true);
+        System.out.println(false || false);
+        System.out.println(x % 5 == 0);
+        System.out.println(y % 5 == 0);
+        System.out.println(z % 5 == 0);
+        System.out.println((x * y * z) > 0);
+        System.out.println((x * y * z) <= 0);
 
     }
 }
