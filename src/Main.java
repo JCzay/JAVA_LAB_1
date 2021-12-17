d0033a5cd0ab04983e614661d8f738dd97a4cece
Julian <Julian.Czaja99@icloud.com>
2021-12-17 22:52:56 +0100

    ćw. 5

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,17 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        double[] tab1 = new double[5];
+        tab1[0] = 1.99;
+        tab1[1] = 2.45;
+        tab1[2] = 3.33;
+        tab1[3] = 4.40;
+        tab1[4] = 5.15;
+        System.out.println(tab1[0]);
+        System.out.println(tab1[1]);
+        System.out.println(tab1[2]);
+        System.out.println(tab1[3]);
+        System.out.println(tab1[4]);
 
     }
 }
